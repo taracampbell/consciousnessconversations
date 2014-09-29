a279fd0e3eaf88fb8e34e0b55f68e334c761f339
Tara Campbell <tara0campbell@gmail.com>
2014-09-29 14:23:58 -0400

    Initial commit.

---
+++ js/grayscale.js
@@ -0,0 +1,9 @@
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
+
+/*$("btn-register").on("click",function(){
+    console.log("Here");
+    window.open('https://www.eventbrite.ca/e/conversations-on-consciousness-tickets-13274366989','_blank');
+});*/
